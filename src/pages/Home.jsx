3b31d6f23231f69945aea32f86c553afb396cde0
SiyyadriDeviramDheeraj <deviramdheeraj1010@gmail.com>
2026-02-23 18:49:51 +0530

    Replaced default Vite template with routing and navbar

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+function Home() {
+  return (
+    <div style={{ padding: "40px", textAlign: "center" }}>
+      <h1>Welcome to Web-Based Resource Library</h1>
+      <p>
+        Search, upload and manage educational resources easily.
+      </p>
+    </div>
+  );
+}
+
+export default Home;
